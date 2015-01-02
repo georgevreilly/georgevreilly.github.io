f7ab961cdaccbc12a375111dfebe4bc05cc2eac4
George V. Reilly <george@reilly.org>
2015-01-01 22:08:45 -0800

    Update documentation

---
+++ content/binary/MindMapSpeech.mm
@@ -0,0 +1,182 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1148258718811" ID="Freemind_Link_41149909" MODIFIED="1168239446243" TEXT="Mind Mapping&#xa;Speech">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="idea"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1168231938040" HGAP="69" ID="Freemind_Link_273931126" MODIFIED="1168242830587" POSITION="right" TEXT="Introduction" VSHIFT="-69">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-1"/>
+<node COLOR="#00b439" CREATED="1168242641415" ID="Freemind_Link_735360439" MODIFIED="1168242650478" TEXT="What is a Mind Map?">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232063978" ID="Freemind_Link_402938190" MODIFIED="1168242671462" TEXT="Uses of mind map">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232111946" ID="Freemind_Link_673966165" MODIFIED="1168242676524" TEXT="Creating mindmaps">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232912696" ID="Freemind_Link_1040840622" MODIFIED="1168232917165" TEXT="Conclude with a demo">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168231844165" HGAP="28" ID="_" MODIFIED="1168242842774" POSITION="right" TEXT="What is a Mind Map?" VSHIFT="-79">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="help"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#00b439" CREATED="1168232247509" ID="Freemind_Link_1357867548" MODIFIED="1168238908696" TEXT="Graphical network of ideas">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232264290" ID="Freemind_Link_430289787" MODIFIED="1168242617696" TEXT="Association of ideas" VSHIFT="-6">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168238928306" ID="Freemind_Link_1351488482" MODIFIED="1168239486868" TEXT="Smells">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="knotify"/>
+</node>
+<node COLOR="#990000" CREATED="1168238931946" ID="Freemind_Link_294074869" MODIFIED="1168238933931" TEXT="Patterns">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168239280899" HGAP="31" ID="Freemind_Link_1490956850" MODIFIED="1168242614524" TEXT="Happiness Example" VSHIFT="-12">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="ksmiletris"/>
+<node COLOR="#990000" CREATED="1168242602696" ID="Freemind_Link_235780522" MODIFIED="1168242622181" TEXT="Chain reaction" VSHIFT="18">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168239271509" ID="Freemind_Link_454294340" MODIFIED="1168239274618" TEXT="Whole Brain">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232481696" ID="Freemind_Link_564811900" MODIFIED="1168238908696" TEXT="Non-linear">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168232488181" ID="Freemind_Link_1316289629" MODIFIED="1168238908696" TEXT="Doesn&apos;t constrain creativity">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168238950946" ID="Freemind_Link_1944846295" MODIFIED="1168239204946" TEXT="Linear Notes">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168239206165" ID="Freemind_Link_1615992923" MODIFIED="1168239222009" TEXT="Must be put in order">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168232503228" ID="Freemind_Link_5630555" MODIFIED="1168238908696" TEXT="Hierarchies">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232510071" ID="Freemind_Link_212572351" MODIFIED="1168238908712" TEXT="Categories">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168231968274" HGAP="27" ID="Freemind_Link_920427576" MODIFIED="1168242867962" POSITION="right" TEXT="Conclusion" VSHIFT="37">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-5"/>
+<node COLOR="#00b439" CREATED="1168231855243" ID="Freemind_Link_739828097" MODIFIED="1168232017540" TEXT="Interactive Demo">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168239180462" ID="Freemind_Link_126123827" MODIFIED="1168239184603" TEXT="Audience Participation">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168242872353" ID="Freemind_Link_1079221104" MODIFIED="1168242878696" TEXT="Wrap Up">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168232288446" HGAP="41" ID="Freemind_Link_297603798" MODIFIED="1168242850118" POSITION="left" TEXT="Uses" VSHIFT="-30">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#00b439" CREATED="1168232329321" HGAP="28" ID="Freemind_Link_1744491018" MODIFIED="1168242455134" TEXT="Brainstorming" VSHIFT="-39">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168239085853" ID="Freemind_Link_64516005" MODIFIED="1168239090946" TEXT="Classic use">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168232371087" ID="Freemind_Link_888567636" MODIFIED="1168238912274" TEXT="Creativity">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168242425618" ID="Freemind_Link_1875895851" MODIFIED="1168242427618" TEXT="Demo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168233959962" ID="Freemind_Link_563833469" MODIFIED="1168238912290" TEXT="Decision Making" VSHIFT="-30">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168239098712" ID="Freemind_Link_1892176533" MODIFIED="1168242438462" TEXT="Lay out factors">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168242441212" ID="Freemind_Link_1008343186" MODIFIED="1168242442540" TEXT="pros">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168242444493" ID="Freemind_Link_1779219939" MODIFIED="1168242445321" TEXT="cons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168242447274" ID="Freemind_Link_466756871" MODIFIED="1168242449259" TEXT="Gestalt">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168232361181" ID="Freemind_Link_145314228" MODIFIED="1168238912290" TEXT="Note Taking">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168232351306" ID="Freemind_Link_648307630" MODIFIED="1168238912290" TEXT="Summarizing someone else&apos;s thoughts">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168232428134" ID="Freemind_Link_1283720134" MODIFIED="1168242477681" TEXT="Standard linear notes - blah">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168239121009" ID="Freemind_Link_780583281" MODIFIED="1168239125399" TEXT="Revision">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168232394524" HGAP="24" ID="Freemind_Link_527681448" MODIFIED="1168239362915" TEXT="Note Making" VSHIFT="29">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168232449743" ID="Freemind_Link_1314277343" MODIFIED="1168238912290" TEXT="Organizing your own thoughts">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168232458259" ID="Freemind_Link_1588931966" MODIFIED="1168238912290" TEXT="Outlining">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168239023696" HGAP="22" ID="Freemind_Link_1190473566" MODIFIED="1168239365790" TEXT="Project Organization" VSHIFT="21">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168239062946" ID="Freemind_Link_1548176416" MODIFIED="1168239068868" TEXT="To-Do Lists">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168239035165" ID="Freemind_Link_838468175" MODIFIED="1168242804415" TEXT="Speech Preparation">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="kaddressbook"/>
+</node>
+<node COLOR="#990000" CREATED="1168239056603" ID="Freemind_Link_1235262748" MODIFIED="1168242794509" TEXT="Writing">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168233980274" HGAP="21" ID="Freemind_Link_1926867383" MODIFIED="1168238912290" TEXT="Improving Memory&#xa;and Imagination" VSHIFT="31">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1168232151493" HGAP="22" ID="Freemind_Link_1379214478" MODIFIED="1168242858337" POSITION="left" TEXT="Creating maps" VSHIFT="30">
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-4"/>
+<node COLOR="#00b439" CREATED="1168232170790" ID="Freemind_Link_471710182" MODIFIED="1168242537962" TEXT="Start with central word/image">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="idea"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232236431" ID="Freemind_Link_101474265" MODIFIED="1168238918118" TEXT="Make associations">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1168232760196" HGAP="21" ID="Freemind_Link_660768577" MODIFIED="1168242509634" TEXT="Images are good" VSHIFT="7">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="wizard"/>
+<node COLOR="#990000" CREATED="1168242484071" ID="Freemind_Link_36894654" MODIFIED="1168242523024" TEXT="Thousand words">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="licq"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1168239141165" ID="Freemind_Link_157638416" MODIFIED="1168239143368" TEXT="Software">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1168239148478" ID="Freemind_Link_824314934" MODIFIED="1168239151712" TEXT="Mindjet">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1168239144524" ID="Freemind_Link_763628778" MODIFIED="1168242551212" TEXT="FreeMind">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="penguin"/>
+</node>
+</node>
+</node>
+</node>
+</map>
